b79c46f0d4e3ca00f407f178a425101f61da2c03
Manoj Kumar <manoj.kumar2@landisgyr.com>
2018-01-11 12:24:29 +0530

    line 2 added by manoj del

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Line2 added by manoj del");
             Console.WriteLine("Line 5 by manoj noida");
             Console.WriteLine("Line 4 by manoj noida");
             Console.WriteLine("Line 3 by manoj noida");
